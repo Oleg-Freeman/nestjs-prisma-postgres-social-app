d5a1e232bd49f6d5d1ce68e56c274214a5fd914b
Oleg Voitiuk <oleg.voitiuk.natife@gmail.com>
2023-08-29 15:30:31 +0300

    configs

---
+++ src/config/index.ts
@@ -0,0 +1,54 @@
+import * as dotenv from 'dotenv';
+import {
+  HttpException,
+  HttpStatus,
+  ValidationPipeOptions,
+} from '@nestjs/common';
+
+dotenv.config();
+
+class ConfigService {
+  constructor(private env: { [k: string]: string | undefined }) {}
+
+  private getValue(key: string, throwOnMissing = true): string {
+    const value = this.env[key];
+
+    if (!value && throwOnMissing) {
+      throw new HttpException(
+        `validation:error. config error - missing env.${key}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+
+    return value;
+  }
+
+  getPort(): number {
+    return +this.getValue('PORT', false) || 3000;
+  }
+
+  getValidationOptions(transform?: true): ValidationPipeOptions {
+    const options: ValidationPipeOptions = {
+      whitelist: true,
+    };
+
+    if (transform) {
+      return {
+        ...options,
+        stopAtFirstError: false,
+        transform: true,
+        forbidNonWhitelisted: false,
+        transformOptions: {
+          enableImplicitConversion: true,
+          exposeDefaultValues: true,
+        },
+      };
+    }
+
+    return options;
+  }
+}
+
+const configService = new ConfigService(process.env);
+
+export { configService };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { configService } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = configService.getPort();
+
+  app
+    .setGlobalPrefix('/v1/api')
+    .useGlobalPipes(new ValidationPipe(configService.getValidationOptions()));
+
+  await app.listen(port, async () =>
+    console.log(`Server is running on: ${await app.getUrl()}`),
+  );
 }
+
 bootstrap();
